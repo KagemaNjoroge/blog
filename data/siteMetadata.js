02b29cf9ee312c63b20fd2e2a798e2c8de5c5820
KagemaNjoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2023-09-11 22:14:41 +0300

    Ensemble metthods

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,6 +19,7 @@ const siteMetadata = {
   youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com/kagemanjoroge',
   locale: 'en-US',
+  
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
