7478afd5388120889859d4a07a22a7a1a3db49e6
Jashanpreet15 <reecejames934@gmail.com>
2024-05-09 14:11:10 +0300

    feat: Add TomorrowAI and UltimatePOS projects
    This commit adds two new projects, TomorrowAI and UltimatePOS, to the projectsData file. TomorrowAI is a startup focused on leveraging AI for data-driven decisions, while UltimatePOS is a modern POS/ERP/CRM system for businesses in Kenya. Both projects have their respective descriptions, images, and links.

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,18 @@
 const projectsData = [
   {
+    title: 'TomorrowAI',
+    description: `A startup my friends and I are working on. We hope to create a platform that will enable businesses to leverage AI to make data-driven decisions. We are currently working on Zoe, an AI assistant that will help business owners get insights about their business. We are so passionate about this project and we believe it will help businesses in Kenya and Africa.`,
+    imgSrc: '/static/images/tomorrowai.svg',
+    href: 'https://tomorrow.co.ke',
+  },
+  {
+    title: 'UltimatePOS',
+    description: `A modern POS/ERP/CRM system for businesses in Kenya. It enables businesses to manage their sales, purchases, inventory, customers, suppliers, and much more. It is built with Django. It also includes Zoe(an AI assistant) that business owners can use to get insights about their business.`,
+    imgSrc: '/static/images/ultimatepos.png',
+    href: 'https://github.com/kagemanjoroge/ultimate_pos',
+  },
+
+  {
     title: 'E-Commerce Store',
     description: `A simple e-commerce store built with Wordpress and woocommerce. It has a simple and clean design that is easy to navigate. It is also mobile responsive.`,
     imgSrc: '/static/images/kyle.png',
@@ -11,13 +24,13 @@ const projectsData = [
     imgSrc: '/static/images/time-machine.jpg',
     href: 'https://github.io/kagemanjoroge',
   },
-    {
+  {
     title: 'Data Persistence in Flutter using SQLite, Book & Authors App',
     description: `A simple app that demonstrates how to use SQLite in Flutter. It is a simple app that allows you to add, edit, delete and view books and authors.`,
     imgSrc: '/static/images/books.png',
     href: 'https://github.com/KagemaNjoroge/BooksAuthorsApp.git',
   },
-      {
+  {
     title: 'ReactJS Portfolio',
     description: `A simple portfolio app built with ReactJS. It is a simple app that showcases my skills and projects.`,
     imgSrc: '/static/images/Demo.png',
