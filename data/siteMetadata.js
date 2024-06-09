9f586aa77b58a206805a1dec178ea1dec4943f58
Kagema Njoroge <137799358+KagemaNjoroge@users.noreply.github.com>
2024-06-09 16:56:02 +0300

    Update siteMetadata.js with Google Analytics ID

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,38 +21,16 @@ const siteMetadata = {
   locale: 'en-US',
 
   analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports Plausible, Simple Analytics, Umami, Posthog or Google Analytics.
-    // umamiAnalytics: {
-    //   // We use an env variable for this site to avoid other users cloning our analytics ID
-    //   umamiWebsiteId: process.env.NEXT_UMAMI_ID, // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
-    // plausibleAnalytics: {
-    //   plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    // },
-    // simpleAnalytics: {},
-    // posthogAnalytics: {
-    //   posthogProjectApiKey: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    // },
     googleAnalytics: {
       googleAnalyticsId: 'G-RK2KMLBNPE', // e.g. G-XXXXXXX
     },
   },
   newsletter: {
-    // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
-    // Please add your .env file and modify it according to your selection
     provider: 'buttondown',
   },
   comments: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // Select a provider and use the environment variables associated to it
-    // https://vercel.com/docs/environment-variables
     provider: 'giscus', // supported providers: giscus, utterances, disqus
     giscusConfig: {
-      // Visit the link below, and follow the steps in the 'configuration' section
-      // https://giscus.app/
       repo: process.env.NEXT_PUBLIC_GISCUS_REPO,
       repositoryId: process.env.NEXT_PUBLIC_GISCUS_REPOSITORY_ID,
       category: process.env.NEXT_PUBLIC_GISCUS_CATEGORY,
@@ -79,15 +57,7 @@ const siteMetadata = {
     kbarConfig: {
       searchDocumentsPath: 'search.json', // path to load documents to search
     },
-    // provider: 'algolia',
-    // algoliaConfig: {
-    //   // The application ID provided by Algolia
-    //   appId: '7J62XWBJIY',
-    //   // Public API key: it is safe to commit it
-    //   apiKey: '50052c47f7a57b02d91522a819a272b7',
-    //   indexName: 'docsearch',
-    // },
   },
 }
 
-module.exports = siteMetadata
+export default siteMetadata
